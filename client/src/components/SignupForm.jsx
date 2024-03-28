204993d5b4d0fade86e07b0e3ebc9f57248c32bd
EdwardBecerra2 <146788503+EdwardBecerra2@users.noreply.github.com>
2024-03-28 15:18:39 -0700

    replaced adduser with ADD_USER

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -12,11 +13,21 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
+  const [addUser, { error }] = useMutation(ADD_USER);
+
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  }, [error]);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
-
+  
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -28,16 +39,13 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
+      const { data } = await addUser(
+        { variables: { ...userFormData },
+        // ...userFormData is the same as {username: userFormData.username, email: userFormData.email, password: userFormData.password} } // ...userFormData is the same as {username: userFormData.username, email: userFormData.email, password: userFormData.password}
+    });
+      console.log(data);
+      Auth.login(data.addUser.token);
+  } catch (err) {
       console.error(err);
       setShowAlert(true);
     }
